0aec122d846fe11904b1aba6ec521279780d20e9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-04-20 13:22:49 -0300

    thumbor-ubuntu: install and run with other engines as well

--- thumbor-ubuntu/install.sh
+++ thumbor-ubuntu/install.sh
@@ -16,11 +16,21 @@ echo "Checking whether OpenCV installed right or not..."
 python -c 'import cv, cv2, cv2.cv' || { echo 'OpenCV did not install correctly' ; exit 1 ; }
 
 # If it's all good, proceed and install Thumbor and OpenCVEngine
-echo "Installing thumbor and opencv-engine..."
-pip install thumbor opencv-engine
+echo "Installing Thumbor with GraphicsMagickEngine and OpenCVEngine..."
+cd /vagrant
+git clone https://github.com/thumbor/thumbor.git
+pip install -e ./thumbor/
+git clone https://github.com/thumbor/graphicsmagick-engine.git
+pip install -e ./graphicsmagick-engine/
+git clone https://github.com/thumbor/opencv-engine.git
+pip install -e ./opencv-engine/
 
-# Start Thumbor on port 8888
-echo "Starting thumbor on port 8888..."
-thumbor -c /vagrant/thumbor.conf -p 8888 &
+# Start Thumbor instances
+echo "Starting Thumbor with GraphicsMagick on port 8881..."
+thumbor -c /vagrant/thumbor-graphicsmagick.conf -p 8881 -l debug > /vagrant/thumbor-graphicsmagick.log &
+echo "Starting Thumbor with OpenCV on port 8882..."
+thumbor -c /vagrant/thumbor-opencv.conf -p 8882 -l debug > /vagrant/thumbor-opencv.log &
+echo "Starting Thumbor with PIL on port 8883..."
+thumbor -c /vagrant/thumbor-pil.conf -p 8883 -l debug > /vagrant/thumbor-pil.log &
 
 echo "Done!"
